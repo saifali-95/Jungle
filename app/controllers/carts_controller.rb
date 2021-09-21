7d40f3be434364b347ae3f6855c563458d9a048a
Saif Ali <saifali7646@gmail.com>
2021-09-21 03:26:01 +0000

    Feature: User Authentication

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,10 +1,13 @@
 class CartsController < ApplicationController
 
+  before_filter :authorize
+  
   def show  
     #Show empty cart and link to render to root if the cart is empty.
     if (cart.size == 0)
       render 'empty'
     else
+  
       render 'show'
     end
   end
